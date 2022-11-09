52b804e6687e8731a9e5a4da00ac3a209c997b0d
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-09 14:53:04 -0300

    load environments settings

--- PrimaryCareDataSheetsApp.ts
+++ PrimaryCareDataSheetsApp.ts
@@ -1,22 +1,26 @@
 import {
   IAppAccessors,
   IConfigurationExtend,
-  IConfigurationModify,
   IEnvironmentRead,
-  ILogger
+  ILogger,
+  ISettingRead
 } from "@rocket.chat/apps-engine/definition/accessors"
 import { App } from "@rocket.chat/apps-engine/definition/App"
 import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata"
+import { Environments } from "./src/domain/entities/environments"
 import { Settings } from "./src/main/config/settings"
 
 export class ObCareDataSheetsApp extends App {
+  public environments: Environments
+  private settingsRead: ISettingRead
+
   constructor (info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
     super(info, logger, accessors)
   }
 
-  override async onEnable (environment: IEnvironmentRead, configurationModify: IConfigurationModify): Promise<boolean> {
+  override async onEnable (): Promise<boolean> {
     try {
-      this.getLogger().log("INITIAL SETTINGS")
+      await this.loadSettings()
       return true
     } catch {
       return false
@@ -26,5 +30,22 @@ export class ObCareDataSheetsApp extends App {
   protected async extendConfiguration (configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
     const settings = new Settings(configuration)
     await settings.createSettings()
+    this.settingsRead = environmentRead.getSettings()
+    await this.readEnvironmentSettings()
+  }
+
+  private async readEnvironmentSettings (): Promise<void> {
+    const envSettings = new Environments({
+      spreadsheetsId: await this.settingsRead.getValueById("SPREADSHEET_ID"),
+      googleCrendentials: await this.settingsRead.getValueById("GOOGLE_CREDENTIALS").then(value => JSON.parse(value))
+    })
+    if (envSettings.isValid()) {
+      this.environments = envSettings.getValue()
+    }
+  }
+
+  private async loadSettings (): Promise<void> {
+    await this.readEnvironmentSettings()
+    this.getLogger().log(`SUCCESSFULLY CONFIGURED ENVIRONMENT`)
   }
 }
