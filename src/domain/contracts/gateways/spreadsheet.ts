5d61c7044d78f8c48b3786d36bae143405264363
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-10 23:41:29 -0300

    add insertDataSheetValues on gateway

--- src/domain/contracts/gateways/spreadsheet.ts
+++ src/domain/contracts/gateways/spreadsheet.ts
@@ -3,10 +3,15 @@ import { Either } from "../../../main/shared/either"
 
 export interface SpreadSheet {
   loadValuesInDataSheet: () => Promise<SpreadSheet.loadValuesInDataSheet.Result>
+  insertValuesInDataSheet: (params: SpreadSheet.insertValuesInDataSheet.Params) => Promise<SpreadSheet.insertValuesInDataSheet.Result>
 }
 
 export namespace SpreadSheet {
   export namespace loadValuesInDataSheet {
     export type Result = Either<InternalServerError, any[]>
+  }
+  export namespace insertValuesInDataSheet {
+    export type Params = { values: { [key: string]: string } }
+    export type Result = Either<InternalServerError, { id: string }>
   }
 }
--- src/infra/gateways/google-spreadsheets.ts
+++ src/infra/gateways/google-spreadsheets.ts
@@ -5,26 +5,56 @@ import { left, right } from "../../main/shared/either"
 import { InternalServerError } from "../errors/internal-server-error"
 
 export class GoogleSpreadSheets implements SpreadSheet {
+  private readonly data: {
+    credentials: {
+      [key: string]: string
+    }
+    fields: string[]
+    spreadSheetId: string
+    spreadSheetTabName: string
+    values?: {
+      [key: string]: string
+    }
+  }
+
   constructor (
     private readonly environments: Environments,
     private readonly httpRequest: IHttp
-  ) {}
+  ) {
+    this.data = {
+      credentials: this.environments.googleCrendentials,
+      fields: this.environments.fieldsHeader,
+      spreadSheetId: this.environments.spreadsheetsId,
+      spreadSheetTabName: this.environments.spreadSheetPageName
+    }
+  }
 
   public async loadValuesInDataSheet (): Promise<SpreadSheet.loadValuesInDataSheet.Result> {
     try {
       const result = await this.httpRequest.post(this.environments.urlApiConnector.concat("/spreadsheet/list"), {
-        data: {
-          credentials: this.environments.googleCrendentials,
-          fields: this.environments.fieldsHeader,
-          spreadSheetId: this.environments.spreadsheetsId,
-          spreadSheetTabName: this.environments.spreadSheetPageName
-        }
+        data: this.data
       })
 
       if (result.statusCode !== 200) {
         return left(new InternalServerError(new Error(`Invalid request with status code ${result.statusCode}`)))
       }
       return right(result.data)
+    } catch (error) {
+      return left(new InternalServerError(error))
+    }
+  }
+
+  public async insertValuesInDataSheet ({ values }: SpreadSheet.insertValuesInDataSheet.Params): Promise<SpreadSheet.insertValuesInDataSheet.Result> {
+    try {
+      this.data.values = values
+      const result = await this.httpRequest.post(this.environments.urlApiConnector.concat("/spreadsheet/add"), {
+        data: this.data
+      })
+
+      if (result.statusCode !== 200) {
+        return left(new InternalServerError(new Error(`Invalid request with status code ${result.statusCode}`)))
+      }
+      return right(result.data.id)
     } catch (error) {
       return left(new InternalServerError(error))
     }
