e7bb0cca6f08976462c9830fa73e4b459ad89775
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-12 20:00:35 -0300

    add defaultValues in contextual bar

--- src/ui/components/contextual-bar.ts
+++ src/ui/components/contextual-bar.ts
@@ -2,9 +2,8 @@ import { IModify } from "@rocket.chat/apps-engine/definition/accessors"
 import { ButtonStyle, TextObjectType } from "@rocket.chat/apps-engine/definition/uikit"
 import { IUIKitContextualBarViewParam } from "@rocket.chat/apps-engine/definition/uikit/UIKitInteractionResponder"
 
-export type ContextualBar = (modify: IModify, fields: string[]) => IUIKitContextualBarViewParam
-
-export const openContextualBar: ContextualBar = (modify: IModify, fields: string[]): IUIKitContextualBarViewParam => {
+export type ContextualBar = (modify: IModify, fields: string[], defaultValues?: object) => IUIKitContextualBarViewParam
+export const openContextualBar: ContextualBar = (modify: IModify, fields: string[], defaultValues?: object): IUIKitContextualBarViewParam => {
   const block = modify.getCreator().getBlockBuilder()
 
   for (const element of fields) {
@@ -12,6 +11,7 @@ export const openContextualBar: ContextualBar = (modify: IModify, fields: string
       blockId: element,
       element: block.newPlainTextInputElement({
         actionId: element,
+        initialValue: defaultValues ? defaultValues[element.toLowerCase()] : undefined,
         placeholder: {
           text: `Fill here with a valid value for ${element.toLowerCase()}`,
           type: TextObjectType.PLAINTEXT
