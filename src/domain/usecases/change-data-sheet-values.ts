127c4eb5c8a683f520acd558a4c66fca3de53cb7
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-13 09:05:52 -0300

    add type Output to changeDataSheet

--- src/domain/usecases/change-data-sheet-values.ts
+++ src/domain/usecases/change-data-sheet-values.ts
@@ -2,15 +2,16 @@ import { Either, left, right } from "../../main/shared/either"
 import { Connector } from "../contracts/gateways/connector"
 
 export type ChangeDataSheet = {
-  perform: () => Promise<Either<string, any[]>>
+  perform: () => Promise<Output>
 }
+type Output = Either<string, any[]>
 
 export class ChangeDataSheetValues {
   constructor (
     private readonly spreadSheetConnector: Connector
   ) {}
 
-  public async perform (): Promise<Either<string, any[]>> {
+  public async perform (): Promise<Output> {
     const data = await this.spreadSheetConnector.loadValuesInDataSheet()
 
     return data.isLeft()
