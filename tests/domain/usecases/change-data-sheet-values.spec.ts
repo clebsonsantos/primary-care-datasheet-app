aa9c943a0475e5ef0d93fc996e21c5274f97f39e
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-13 00:07:26 -0300

    ensured call usecase ChangeDataSheetValues with error if return to equal internalServerError

--- tests/domain/usecases/change-data-sheet-values.spec.ts
+++ tests/domain/usecases/change-data-sheet-values.spec.ts
@@ -3,16 +3,19 @@ import { assert, expect } from "chai";
 import { describe, it, beforeEach } from "mocha";
 import { Connector } from "../../../src/domain/contracts/gateways/connector";
 import { StubbedInstance, stubInterface } from "ts-sinon";
-import { right } from "../../../src/main/shared/either";
+import { left, right } from "../../../src/main/shared/either";
+import { InternalServerError } from "../../../src/infra/errors/internal-server-error";
 
 describe("ChangeDataSheetValues", () => {
     let sut: ChangeDataSheetValues
     let spreadConnector: StubbedInstance<Connector>
 
-    beforeEach(() => {
+    before(()=> {
         spreadConnector = stubInterface<Connector>({
-            loadValuesInDataSheet: new Promise(resolve => resolve(right([])))
+            loadValuesInDataSheet: Promise.resolve(right([]))
         })
+    })
+    beforeEach(() => {
         sut = new ChangeDataSheetValues(spreadConnector)
     })
 
@@ -21,6 +24,16 @@ describe("ChangeDataSheetValues", () => {
         expect(result.isRight()).to.be.ok
         expect(result.isLeft()).to.be.not.ok
         result.isRight() && assert.instanceOf(result.value, Array)
+    })
+
+    it("should call ChangeDataSheetValues and return error if Internal server Error", async () => {
+        const internalError = new InternalServerError(new Error("internal server error"))
+        spreadConnector.loadValuesInDataSheet.resolves(left(internalError))
+
+        const result = await sut.perform()
+        expect(result.isLeft()).to.be.ok
+        expect(result.isRight()).to.be.not.ok
+        result.isLeft() && assert.typeOf(result.value, "string")
     })
 
 })
